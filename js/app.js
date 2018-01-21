ea087915d4e5e9e32029ef923638de391de98981
Honriri <632858620@qq.com>
2018-01-21 13:00:21 +0100

    1-21
    Basic layout

--- js/app.js
+++ js/app.js
@@ -11,5 +11,12 @@ $(function() {
 	 * In other places you should limit the search only to the children 
 	 * of the specific view you're working with (see exampleView.js).
 	 */
+	 /**
+	 * IMPORTANT: app.js is the only place where you are allowed to
+	 * use the $('someSelector') to search for elements in the whole HTML.
+	 * In other places you should limit the search only to the children 
+	 * of the specific view you're working with (see exampleView.js).
+	 */
+
 
 });
