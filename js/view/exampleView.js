5373f64ca06dab997ab75a8e810c5f671bbde61a
Honriri <632858620@qq.com>
2018-01-25 15:10:31 +0100

    1.25
    Add responsively menu

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -12,6 +12,9 @@
  * @param {jQuery object} container - references the HTML parent element that contains the view.
  * @param {Object} model - the reference to the Dinner Model
  */ 
+
+
+
 var ExampleView = function (container, model) {
 	
 	/**
