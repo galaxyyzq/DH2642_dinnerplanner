2cf641d143e8b46d040c42d8377b762cfd7c95c2
galaxyyzq <33658860+galaxyyzq@users.noreply.github.com>
2018-02-01 09:14:33 +0100

    rewrite finalMenuView

--- js/view/finalMenuView.js
+++ js/view/finalMenuView.js
@@ -2,71 +2,82 @@ var FinalMenuView = function(container, model) {
 	var finalMenu = container.find("#finalMenu");
 	var totalCost = container.find("#totalCost");
 
-	var div = document.createElement('div');
-	div.className ="ol-md-12 dishoverview";
+	var div = document.createElement('DIV');
+	div.className ="col-md-12 dishoverview";
 
 	var totalCost = 0;
+	var cost = 0;
 
 	for(var i = 0; i < model.getFullMenu().length; i++){
 
-		var element = document.createElement('DIV');
-		element.className = "thumbnail";
-		element.style = "margin: 0 10px 0 10px"
+		var col = document.createElement('DIV');
+		col.className = "thumbnail";
+		col.style = "margin: 0 10px 0 10px";
 
-		var cost = 0;
 
-		var divName = document.createElement('DIV');
-		divName.className ="name";
-		divName.innerHTML = model.getFullMenu()[i].name;
 		var img = document.createElement('img');
+		img.className = "dishpic";
+		img.style = "margin: 0 10px 0 10px";
 		var src = "images/" + model.getFullMenu()[i].image;
 		img.setAttribute("src", src);
-		img.className = "dishpic"
-		img.style = "margin: 0 10px 0 10px"
-		var divDish = document.createElement('DIV');
-		divDish.className = "dish";
-		divDish.appendChild(img);
-		divDish.appendChild(divName);
+		col.appendChild(img);
 
-		// <div class="col-md-12 dishoverview" >
-        //<div class="thumbnail" style="margin: 0 10px 0 10px">
-        //      <img src="images/meatballs.jpg" class="dishpic">
-        //     <div class="caption text-center">
-        //            <h4>Meatballs</h4>
-        //            <p>20 SEK</p>
-        //      </div>
-        //</div>
+		var divText = document.createElement('DIV');
+        divText.className ="caption text-center";
 
-		element.appendChild(divDish);
+        var divName = document.createElement('DIV');
+        divName.innerHTML = model.getFullMenu()[i].name;
+        divText.appendChild(divName);
 
-		for(var j = 0; j < model.getFullMenu()[i].ingredients.length; j++){
-			cost += model.getFullMenu()[i].ingredients[j].price * model.getFullMenu()[i].ingredients[j].quantity * model.getNumberOfGuests();
+ 		for(var j = 0; j < model.getFullMenu()[i].ingredients.length; j++){
+			cost += model.getFullMenu()[i].ingredients[j].price * model.getNumberOfGuests();
 		}
 
 		totalCost += cost;
 
-		divCost = document.createElement('H4');
+		var divCost = document.createElement('H4');
 		divCost.innerHTML = cost + " SEK";
+		divText.appendChild(divCost);
+		col.appendChild(divText);
 
-		element.appendChild(divCost);
-		div.appendChild(element);
-	}
+		div.appendChild(col);
 
-	//    <div class="col-md-12 text-center" style="padding: 20px 0 20px 0">
-    //  <div style="margin: 0 20px 0 20px">
-    //        <h3>Total</h3>
-    //        <p>90 SEK</p>
-    //  </div>
-	
+}
 
-	var costElt = document.createElement('DIV');
-	costElt.className = "col-md-12 text-center";
-	var cost = document.createElement('H3');
-	cost.className = "center";
-	cost.innerHTML = "Total： " + totalCost + " SEK";
-	costElt.appendChild(cost);
+	var costAll = document.createElement('DIV');
+	costAll.className = "col-md-12 text-center";
+	var costNumber = document.createElement('H3');
+	costNumber.className = "center";
+	costNumber.innerHTML = "Total： " + totalCost + " SEK";
+	costAll.appendChild(costNumber);
 
-	div.appendChild(costElt);
+    div.appendChild(costAll);
+
 	finalMenu.append(div);
 	
 }
+
+//original HTML
+//<div class="col-md-12 dishoverview" >
+//        <div class="thumbnail" style="margin: 0 10px 0 10px">
+//              <img src="images/meatballs.jpg" class="dishpic">
+//              <div class="caption text-center">
+//                    <h4>Meatballs</h4>
+//                    <p>20 SEK</p>
+//              </div>
+//        </div>
+//        <div class="thumbnail" style="margin: 0 10px 0 10px">
+//                  <img src="images/icecream.jpg" class="dishpic">
+//                  <div class="caption text-center">
+//                    <h4>Icecream</h4>
+//                    <p>30 SEK</p>
+//                </div>
+//          </div>
+//          <div class="thumbnail" style="margin: 0 10px 0 10px">
+//                  <img src="images/sourdough.jpg" class="dishpic">
+//                  <div class="caption text-center">
+//                    <h4>Sourdough</h4>
+//                    <p>30 SEK</p>
+//                </div>
+//          </div>
+//    </div>
