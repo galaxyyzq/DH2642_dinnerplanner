916f31161333b5aac7aff40295c65274fcf1d9c5
Honriri <632858620@qq.com>
2018-02-14 15:01:24 +0100

    Detail button controller

--- js/controller/DishViewController.js
+++ js/controller/DishViewController.js
@@ -1,13 +1,19 @@
 var DishViewController = function(view1,view2, model){
 
-  var btn_detail = document.getElementsByClassName("detailbutton");
-  var listner = function(){
-    view1.hide();
-    view2.show();
-  }
+$("#dishView").on("click",".detailbutton",function(){
+  view1.hide();
+  view2.show();
+});
 
-  for(var i = 0; i < btn_detail.length; i++){
-    btn_detail[i].addEventListener("click",listner,false);
-  }
+  //
+  // var btn_detail = document.getElementsByClassName("detailbutton");
+  // var listner = function(){
+  //   view1.hide();
+  //   view2.show();
+  // }
+  //
+  // for(var i = 0; i < btn_detail.length; i++){
+  //   btn_detail[i].addEventListener("click",listner,false);
+  // }
 
 }
