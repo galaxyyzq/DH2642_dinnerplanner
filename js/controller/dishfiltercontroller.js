75954f22801ca49b44c4c3cc9e87ad969cd9fed7
Honriri <632858620@qq.com>
2018-02-21 12:14:25 +0100

    Add search function
    Add search function

--- js/controller/dishfiltercontroller.js
+++ js/controller/dishfiltercontroller.js
@@ -2,13 +2,26 @@
 var DishFilterController = function(view, model){
 
   var filter= $("#filter").get(0);
+  var search= $("#search").get(0);
 
-  var listener = function(){
-    var val =$("#filter").val();
-    view.types = val;
-    view.loadDishView(val);
+  var listener_filter = function(){
+    var typeVal =$("#filter").val();
+    var nameVal = $("#search").val();
+    view.types = typeVal;
+    view.searchnames = nameVal;
+    view.loadDishView(typeVal,nameVal);
   }
 
-  filter.addEventListener("change",listener,false);
+  var listener_search = function(){
+    var nameVal = $("#search").val();
+      var typeVal =$("#filter").val();
+    view.searchnames = nameVal;
+    view.types = typeVal;
+    view.loadDishView(typeVal,nameVal)
+
+  }
+
+  filter.addEventListener("change",listener_filter,false);
+  search.addEventListener("input", listener_search, false);
 
 }
--- js/view/dishView.js
+++ js/view/dishView.js
@@ -12,61 +12,67 @@ var DishView = function (container, model) {
 	var dishes = container.find("#dish");
 	var parent = this;
 	this.types="starter";
+	this.searchnames ="";
 
 
 	//Construct View Function
-	this.loadDishView = function(types){
+	this.loadDishView = function(types,searchnames){
 
 		dishes.html("");
 
 		var div = document.createElement('DIV');
 
 		 type = types;
-
-		 var detailbuttonListener =function(dishid){
-			 loadDishDetailView()
-		 }
+		 // searchname= searchnames;
 
 		for(i = 0; i < model.getSelectedDish(type).length; i++){
 
-			var col = document.createElement('DIV');
-			col.className = "col-sm-4";
+			var name=model.getSelectedDish(type)[i].name;
+			var searchResult=name.indexOf(searchnames);
 
-			var thumbnail = document.createElement('DIV');
-			thumbnail.className = "thumbnail";
-			var img = document.createElement('img');
-			img.className = "dishpic";
-			var src = "images/" + model.getSelectedDish(type)[i].image;
-			img.setAttribute("src", src);
+			if (searchResult>-1) {
+				var col = document.createElement('DIV');
+				col.className = "col-sm-4";
 
-			var divD = document.createElement('DIV');
-			divD.className = "dish text-center";
-			divD.appendChild(img);
+				var thumbnail = document.createElement('DIV');
+				thumbnail.className = "thumbnail";
+				var img = document.createElement('img');
+				img.className = "dishpic";
+				var src = "images/" + model.getSelectedDish(type)[i].image;
+				img.setAttribute("src", src);
 
-			var name = document.createElement('DIV');
-			name.className = "caption text-center";
-			var h4name = document.createElement('H4');
-			h4name.innerHTML = model.getSelectedDish(type)[i].name;
-			name.appendChild(h4name);
+				var divD = document.createElement('DIV');
+				divD.className = "dish text-center";
+				divD.appendChild(img);
 
-			divD.appendChild(name);
+				var name = document.createElement('DIV');
+				name.className = "caption text-center";
+				var h4name = document.createElement('H4');
+				h4name.innerHTML = model.getSelectedDish(type)[i].name;
+				name.appendChild(h4name);
+
+				divD.appendChild(name);
 
 
 
-			var btn_detail = document.createElement('DIV');
-			btn_detail.innerHTML = "<button href='#' class='btn btn-defult btn-sm' role='button'>Detail</button>";
-			btn_detail.className = "detailbutton";
+				var btn_detail = document.createElement('DIV');
+				btn_detail.innerHTML = "<button href='#' class='btn btn-defult btn-sm' role='button'>Detail</button>";
+				btn_detail.className = "detailbutton";
 
-			btn_detail.className = model.getSelectedDish(type)[i].id;
-			divD.appendChild(btn_detail);
+				btn_detail.className = model.getSelectedDish(type)[i].id;
+				divD.appendChild(btn_detail);
 
-			// var btn_add = document.createElement('DIV');
-			// btn_add.innerHTML = "<button href='#' class='btn btn-info btn-sm' role='button'>+  Add</button>";
-			// divD.appendChild(btn_add);
+				// var btn_add = document.createElement('DIV');
+				// btn_add.innerHTML = "<button href='#' class='btn btn-info btn-sm' role='button'>+  Add</button>";
+				// divD.appendChild(btn_add);
 
-			thumbnail.appendChild(divD);
-			col.appendChild(thumbnail);
-			div.appendChild(col);
+				thumbnail.appendChild(divD);
+				col.appendChild(thumbnail);
+				div.appendChild(col);
+
+			}
+
+
 		}
 
 		dishes.append(div);
@@ -75,10 +81,10 @@ var DishView = function (container, model) {
 
 
 	this.update = function() {
-		parent.loadDishView(parent.types);
+		parent.loadDishView(parent.types, parent.searchnames);
 	}
 	model.addObserver(this);
 
-	this.loadDishView(this.types);
+	this.loadDishView(this.types, parent.searchnames);
 
 }
